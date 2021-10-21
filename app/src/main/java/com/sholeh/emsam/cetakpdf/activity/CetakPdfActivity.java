d0dc725f54670cde9c6a27636df5b290ff2424a0
Reindra12 <reindrairawan@gmail.com>
2021-10-21 16:15:18 +0700

    buat profil

---
+++ app/src/main/java/com/sholeh/emsam/cetakpdf/activity/CetakPdfActivity.java
@@ -0,0 +1,19 @@
+package com.sholeh.emsam.cetakpdf.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.androidnetworking.AndroidNetworking;
+import com.sholeh.emsam.R;
+
+public class CetakPdfActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cetak_pdf);
+
+        AndroidNetworking.initialize(getApplicationContext());
+    }
+}
