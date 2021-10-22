e58846f7f1452537e695df72113beaa3b1b30fb9
argassholeh <attaftazanisholeh@gmail.com>
2021-10-22 23:55:47 +0700

    logout in home fragment

--- app/src/main/java/com/sholeh/emsam/mfragment/HomeFrgament.java
+++ app/src/main/java/com/sholeh/emsam/mfragment/HomeFrgament.java
@@ -4,6 +4,7 @@
 
 package com.sholeh.emsam.mfragment;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
@@ -14,17 +15,22 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.developer.kalert.KAlertDialog;
+import com.kaopiz.kprogresshud.KProgressHUD;
+import com.sholeh.emsam.ActivityLogin;
 import com.sholeh.emsam.Adapter.AdapterKaryawan;
 import com.sholeh.emsam.Api.BaseApiService;
 import com.sholeh.emsam.Api.UrlApi;
+import com.sholeh.emsam.Helper.Preferences;
 import com.sholeh.emsam.Model.ItemKaryawan;
 import com.sholeh.emsam.Model.ResponseKaryawan;
 import com.sholeh.emsam.R;
@@ -37,43 +43,46 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 
-public class HomeFrgament extends Fragment {
+public class HomeFrgament extends Fragment implements View.OnClickListener {
 
     TextView tvx_title, tvx_logout;
     ImageView imgtoolbar;
-    private List<ItemKaryawan> listDataKaryawan = new ArrayList<>();;
+    private List<ItemKaryawan> listDataKaryawan = new ArrayList<>();
+    ;
     private AdapterKaryawan adapter;
     BaseApiService ApiService;
     RecyclerView rv_karyawan;
     KAlertDialog pDialog;
-
-
+    private KProgressHUD progressDialogHud;
+    Preferences preferences;
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_home,container,false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
 
-            imgtoolbar = rootView.findViewById(R.id.imgtoolbarFBack);
-            imgtoolbar.setVisibility(View.GONE);
-            tvx_title = rootView.findViewById(R.id.tvx_title);
-            tvx_logout = rootView.findViewById(R.id.logout_akun);
-            rv_karyawan = rootView.findViewById(R.id.recycler_karyawan);
+        imgtoolbar = rootView.findViewById(R.id.imgtoolbarFBack);
+        imgtoolbar.setVisibility(View.GONE);
+        tvx_title = rootView.findViewById(R.id.tvx_title);
+        tvx_logout = rootView.findViewById(R.id.logout_akun);
+        rv_karyawan = rootView.findViewById(R.id.recycler_karyawan);
+        progressDialogHud = KProgressHUD.create(getActivity());
+        preferences = new Preferences(getActivity());
+
 
         tvx_title.setText("Data Karyawan");
         tvx_title.setTypeface(tvx_title.getTypeface(), Typeface.BOLD);
         tvx_title.setTextColor(Color.parseColor("#FFFFFF"));
-        tvx_logout.setVisibility(View.GONE);
+//        tvx_logout.setVisibility(View.GONE);
 
         ApiService = UrlApi.getAPIService();
         adapter = new AdapterKaryawan(getActivity(), listDataKaryawan);
         RecyclerView.LayoutManager LayoutManager = new LinearLayoutManager(getActivity());
         rv_karyawan.setLayoutManager(LayoutManager);
         rv_karyawan.setHasFixedSize(true);
-
-
         getResultList();
+        tvx_logout.setOnClickListener(this);
         return rootView;
     }
 
@@ -120,6 +129,51 @@ public class HomeFrgament extends Fragment {
             }
         });
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.logout_akun:
+                logoutAkun();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public void logoutAkun() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Apakah anda yakin, ingin logout?");
+        builder.setCancelable(true);
+        builder.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ProgresDialog();
+                preferences.saveSPBoolean(preferences.SP_SUDAH_LOGIN, false);
+                startActivity(new Intent(getActivity(), ActivityLogin.class)
+                        .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
+                getActivity().finish();
+                Toast.makeText(getActivity(), "Berhasil Keluar", Toast.LENGTH_LONG).show();
+                progressDialogHud.dismiss();
+            }
+        });
+        builder.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void ProgresDialog() {
+        progressDialogHud.setStyle(KProgressHUD.Style.SPIN_INDETERMINATE)
+                .setCancellable(false);
+        progressDialogHud.show();
+    }
+
 
 }
 
